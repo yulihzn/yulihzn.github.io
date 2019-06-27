9945a2c42e185269bda20e5e0de8953476c6d45c
yulihzn <330193303@qq.com>
2019-06-27 22:22:14 +0800

    fixed

--- web-mobile/main.2bb4d.js
+++ web-mobile/main.2bb4d.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.1e7df.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.6b027.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.e318d.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.2bb4d.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
