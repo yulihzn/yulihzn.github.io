8755ddf92427068d308d8820251eb22970e3e30c
yulihzn <yuli.he@99bill.com>
2019-06-27 21:21:28 +0800

    fix

--- web-mobile/main.e318d.js
+++ web-mobile/main.e318d.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.a015e.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.1e7df.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.001ab.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.e318d.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -200,12 +200,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.6254c.js');
+        require('src/settings.7adcb.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.6254c.js');
+        require('src/settings.7adcb.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
