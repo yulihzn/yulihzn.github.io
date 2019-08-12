7d0cdc6693239329fc3572119b32873c63d273da
yulihzn <330193303@qq.com>
2019-08-13 00:15:34 +0800

    circle

--- web-mobile1/main.1c487.js
+++ web-mobile1/main.1c487.js
@@ -144,7 +144,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8cb4d.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.83509.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -180,7 +180,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.caa61.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.1c487.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -207,12 +207,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.b5b8a.js');
+        require('src/settings.6f291.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.b5b8a.js');
+        require('src/settings.6f291.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
