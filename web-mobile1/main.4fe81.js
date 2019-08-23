eefbfff156298416ca9c9e4d30782d9e4fd05e66
yulihzn <yuli.he@99bill.com>
2019-08-23 19:41:29 +0800

    revolute

--- web-mobile1/main.4fe81.js
+++ web-mobile1/main.4fe81.js
@@ -144,7 +144,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.f9c8b.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.e2eb3.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -180,7 +180,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.aad61.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.4fe81.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -207,12 +207,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.14b01.js');
+        require('src/settings.68eb1.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.14b01.js');
+        require('src/settings.68eb1.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
